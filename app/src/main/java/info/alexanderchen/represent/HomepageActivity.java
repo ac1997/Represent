7a1d926c8fc634e3ee2a335659475ef569741536
Alexander Chen <alexanderchen@alexanders-mbp.attlocal.net>
2018-09-29 10:42:08 -0700

    Search bar partial done

--- app/src/main/java/info/alexanderchen/represent/HomepageActivity.java
+++ app/src/main/java/info/alexanderchen/represent/HomepageActivity.java
@@ -1,83 +1,281 @@
 package info.alexanderchen.represent;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.SearchSuggestionsAdapter;
 import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
+import com.arlib.floatingsearchview.util.Util;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import info.alexanderchen.represent.adapter.SearchResultsListAdapter;
+import info.alexanderchen.represent.data.ColorSuggestion;
+import info.alexanderchen.represent.data.ColorWrapper;
+import info.alexanderchen.represent.data.DataHelper;
 
 public class HomepageActivity extends AppCompatActivity {
 
-    FloatingSearchView mSearchView;
+    private final String TAG = "BlankFragment";
 
-    private static final List<String> SOME_HARDCODED_DATA;
-    static {
-        SOME_HARDCODED_DATA = new ArrayList<>();
-        SOME_HARDCODED_DATA.add("One");
-        SOME_HARDCODED_DATA.add("Two");
-        SOME_HARDCODED_DATA.add("Three");
-        SOME_HARDCODED_DATA.add("Four");
-    }
-    private static class SimpleSuggestions implements SearchSuggestion {
-        private final String mData;
-        public SimpleSuggestions(String string) {
-            mData = string;
-        }
-        @Override
-        public String getBody() {
-            return mData;
-        }
+    public static final long FIND_SUGGESTION_SIMULATED_DELAY = 250;
 
-        @Override
-        public int describeContents() {
-            return 0;
-        }
+    private FloatingSearchView mSearchView;
 
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeString(mData);
-        }
+    private RecyclerView mSearchResultsList;
+    private SearchResultsListAdapter mSearchResultsAdapter;
 
-        public static final Parcelable.Creator<SimpleSuggestions> CREATOR
-                = new Parcelable.Creator<SimpleSuggestions>() {
-            public SimpleSuggestions createFromParcel(Parcel in) {
-                return new SimpleSuggestions(in);
-            }
+    private boolean mIsDarkSearchTheme = false;
 
-            public SimpleSuggestions[] newArray(int size) {
-                return new SimpleSuggestions[size];
-            }
-        };
-
-        private SimpleSuggestions(Parcel in) {
-            mData = in.readString();
-        }
-    }
+    private String mLastQuery = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
 
-        mSearchView = findViewById(R.id.floating_search_view);
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
+        mSearchResultsList = (RecyclerView) findViewById(R.id.search_results_list);
+
+        setupFloatingSearch();
+        setupResultsList();
+    }
+
+    private void setupFloatingSearch() {
         mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
+
             @Override
             public void onSearchTextChanged(String oldQuery, final String newQuery) {
-                List<SearchSuggestion> list = new ArrayList<SearchSuggestion>();
-                //emulating search on dummy data
-                for (String item : SOME_HARDCODED_DATA) {
-                    if (item.contains(newQuery)) {
-                        list.add(new SimpleSuggestions(item));
-                    }
+
+                if (!oldQuery.equals("") && newQuery.equals("")) {
+                    mSearchView.clearSuggestions();
+                } else {
+
+                    //this shows the top left circular progress
+                    //you can call it where ever you want, but
+                    //it makes sense to do it when loading something in
+                    //the background.
+                    mSearchView.showProgress();
+
+                    //simulates a query call to a data source
+                    //with a new query.
+                    DataHelper.findSuggestions(null, newQuery, 5,
+                            FIND_SUGGESTION_SIMULATED_DELAY, new DataHelper.OnFindSuggestionsListener() {
+
+                                @Override
+                                public void onResults(List<ColorSuggestion> results) {
+
+                                    //this will swap the data and
+                                    //render the collapse/expand animations as necessary
+                                    mSearchView.swapSuggestions(results);
+
+                                    //let the users know that the background
+                                    //process has completed
+                                    mSearchView.hideProgress();
+                                }
+                            });
                 }
-                mSearchView.swapSuggestions(list);
+
+                Log.d(TAG, "onSearchTextChanged()");
             }
         });
+
+        mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
+            @Override
+            public void onSuggestionClicked(final SearchSuggestion searchSuggestion) {
+
+                ColorSuggestion colorSuggestion = (ColorSuggestion) searchSuggestion;
+                DataHelper.findColors(getApplicationContext(), colorSuggestion.getBody(),
+                        new DataHelper.OnFindColorsListener() {
+
+                            @Override
+                            public void onResults(List<ColorWrapper> results) {
+                                mSearchResultsAdapter.swapData(results);
+                            }
+
+                        });
+                Log.d(TAG, "onSuggestionClicked()");
+
+                mLastQuery = searchSuggestion.getBody();
+            }
+
+            @Override
+            public void onSearchAction(String query) {
+                mLastQuery = query;
+
+                DataHelper.findColors(getApplicationContext(), query,
+                        new DataHelper.OnFindColorsListener() {
+
+                            @Override
+                            public void onResults(List<ColorWrapper> results) {
+                                mSearchResultsAdapter.swapData(results);
+                            }
+
+                        });
+                Log.d(TAG, "onSearchAction()");
+            }
+        });
+
+        mSearchView.setOnFocusChangeListener(new FloatingSearchView.OnFocusChangeListener() {
+            @Override
+            public void onFocus() {
+
+                //show suggestions when search bar gains focus (typically history suggestions)
+                mSearchView.swapSuggestions(DataHelper.getHistory(getApplicationContext(), 3));
+
+                Log.d(TAG, "onFocus()");
+            }
+
+            @Override
+            public void onFocusCleared() {
+
+                //set the title of the bar so that when focus is returned a new query begins
+                mSearchView.setSearchBarTitle(mLastQuery);
+
+                //you can also set setSearchText(...) to make keep the query there when not focused and when focus returns
+                //mSearchView.setSearchText(searchSuggestion.getBody());
+
+                Log.d(TAG, "onFocusCleared()");
+            }
+        });
+
+
+        //handle menu clicks the same way as you would
+        //in a regular activity
+        mSearchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
+            @Override
+            public void onActionMenuItemSelected(MenuItem item) {
+
+                if (item.getItemId() == R.id.action_location) {
+
+                    mIsDarkSearchTheme = true;
+
+                    //demonstrate setting colors for items
+                    mSearchView.setBackgroundColor(Color.parseColor("#787878"));
+                    mSearchView.setViewTextColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setHintTextColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setActionMenuOverflowColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setMenuItemIconColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setClearBtnColor(Color.parseColor("#e9e9e9"));
+                    mSearchView.setDividerColor(Color.parseColor("#BEBEBE"));
+                    mSearchView.setLeftActionIconColor(Color.parseColor("#e9e9e9"));
+                } else {
+
+                    //just print action
+                    Toast.makeText(getApplicationContext().getApplicationContext(), item.getTitle(),
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        //use this listener to listen to menu clicks when app:floatingSearch_leftAction="showHome"
+        mSearchView.setOnHomeActionClickListener(new FloatingSearchView.OnHomeActionClickListener() {
+            @Override
+            public void onHomeClicked() {
+
+                Log.d(TAG, "onHomeClicked()");
+            }
+        });
+
+        /*
+         * Here you have access to the left icon and the text of a given suggestion
+         * item after as it is bound to the suggestion list. You can utilize this
+         * callback to change some properties of the left icon and the text. For example, you
+         * can load the left icon images using your favorite image loading library, or change text color.
+         *
+         *
+         * Important:
+         * Keep in mind that the suggestion list is a RecyclerView, so views are reused for different
+         * items in the list.
+         */
+        mSearchView.setOnBindSuggestionCallback(new SearchSuggestionsAdapter.OnBindSuggestionCallback() {
+            @Override
+            public void onBindSuggestion(View suggestionView, ImageView leftIcon,
+                                         TextView textView, SearchSuggestion item, int itemPosition) {
+                ColorSuggestion colorSuggestion = (ColorSuggestion) item;
+
+                String textColor = mIsDarkSearchTheme ? "#ffffff" : "#000000";
+                String textLight = mIsDarkSearchTheme ? "#bfbfbf" : "#787878";
+
+                if (colorSuggestion.getIsHistory()) {
+                    leftIcon.setImageDrawable(ResourcesCompat.getDrawable(getResources(),
+                            R.drawable.ic_history_black_24dp, null));
+
+                    Util.setIconColor(leftIcon, Color.parseColor(textColor));
+                    leftIcon.setAlpha(.36f);
+                } else {
+                    leftIcon.setAlpha(0.0f);
+                    leftIcon.setImageDrawable(null);
+                }
+
+                textView.setTextColor(Color.parseColor(textColor));
+                String text = colorSuggestion.getBody()
+                        .replaceFirst(mSearchView.getQuery(),
+                                "<font color=\"" + textLight + "\">" + mSearchView.getQuery() + "</font>");
+                textView.setText(Html.fromHtml(text));
+            }
+
+        });
+
+        //listen for when suggestion list expands/shrinks in order to move down/up the
+        //search results list
+        mSearchView.setOnSuggestionsListHeightChanged(new FloatingSearchView.OnSuggestionsListHeightChanged() {
+            @Override
+            public void onSuggestionsListHeightChanged(float newHeight) {
+                mSearchResultsList.setTranslationY(newHeight);
+            }
+        });
+
+        /*
+         * When the user types some text into the search field, a clear button (and 'x' to the
+         * right) of the search text is shown.
+         *
+         * This listener provides a callback for when this button is clicked.
+         */
+        mSearchView.setOnClearSearchActionListener(new FloatingSearchView.OnClearSearchActionListener() {
+            @Override
+            public void onClearSearchClicked() {
+
+                Log.d(TAG, "onClearSearchClicked()");
+            }
+        });
+    }
+
+    private void setupResultsList() {
+        mSearchResultsAdapter = new SearchResultsListAdapter();
+        mSearchResultsList.setAdapter(mSearchResultsAdapter);
+        mSearchResultsList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+
+    @Override
+    public void onBackPressed() {
+        //if mSearchView.setSearchFocused(false) causes the focused search
+        //to close, then we don't want to close the activity. if mSearchView.setSearchFocused(false)
+        //returns false, we know that the search was already closed so the call didn't change the focus
+        //state and it makes sense to call supper onBackPressed() and close the activity
+        super.onBackPressed();
+//        if (!mSearchView.setSearchFocused(false)) {
+//            return false;
+//        }
+//        return true;
     }
 }
